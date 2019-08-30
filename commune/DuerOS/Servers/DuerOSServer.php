0164c2bfed776c2c4e5531ac7ed28fd13e2a82c4
thirdgerb <thirdgerb@gmail.com>
2019-08-30 18:15:28 +0800

    测试dueros链接

--- commune/DuerOS/Servers/DuerOSServer.php
+++ commune/DuerOS/Servers/DuerOSServer.php
@@ -10,12 +10,28 @@ namespace Commune\DuerOS\Servers;
 
 use Baidu\Duer\Botsdk\Request;
 use Baidu\Duer\Botsdk\Response;
+use Commune\Hyperf\Foundations\Dependencies\StdConsoleLogger;
 use Hyperf\Contract\OnRequestInterface;
 use Swoole\Http\Request as SwooleRequest;
 use Swoole\Http\Response as SwooleResponse;
 
 class DuerOSServer implements OnRequestInterface
 {
+    /**
+     * @var StdConsoleLogger
+     */
+    protected $logger;
+
+    /**
+     * DuerOSServer constructor.
+     * @param StdConsoleLogger $logger
+     */
+    public function __construct(StdConsoleLogger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+
     public function onRequest(SwooleRequest $request, SwooleResponse $response): void
     {
         // prepare duer os bot request
@@ -32,6 +48,8 @@ class DuerOSServer implements OnRequestInterface
         );
 
         $output = $botResponse->build(['outputSpeech' => '你好']);
+
+        $this->logger->info($output);
 
         $response->end($output);
     }
